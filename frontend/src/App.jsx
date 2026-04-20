2b75c5f798fa4c84b18b18b7ca0c51e88070a391
Анастасия Ковырзина <kovyrzina06@mail.ru>
2026-04-20 13:35:26 +0500

    Добавлена верстка страниц планировщика бюджета и определения породы

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,13 @@ import AuthorizationPage from "./pages/AuthorizationPage";
 import ArticlesPage from "./pages/ArticlesPage";
 import ArticlePage from "./pages/ArticlePage";
 import AppointmentPage from "./pages/AppointmentPage";
-import BudgetPlanPage from "./pages/BudgetPlanPage";
+import BudgetPlanPage from "./pages/BudgetPlannerPage";
 import CabinetPage from "./pages/CabinetPage";
 import RegistrationPage from "./pages/RegistrationPage";
 import CreateArticlePage from "./pages/CreateArticlePage";
+import BreedIdentificationPage from './pages/BreedIdentificationPage';
+import BudgetPlannerPage from './pages/BudgetPlannerPage';
+
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
@@ -38,9 +41,13 @@ function App() {
         </PrivateRoute>
       } />
       <Route path="/create-article" element={
-        <PrivateRoute>
           <CreateArticlePage />
-        </PrivateRoute>
+      } />
+      <Route path="/breed-identification" element={
+        <BreedIdentificationPage/>
+      } />
+      <Route path="/budget-planner" element={
+        <BudgetPlanPage/>
       } />
     </Routes>
   );
